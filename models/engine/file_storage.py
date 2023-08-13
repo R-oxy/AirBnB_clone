703b3bd3469827e17562af17e4673f2822b543dc
R-oxy <aronmangati@gmail.com>
2023-08-13 12:53:26 -0700

    Updated FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 Hbnb clone FileStorage class definition.
 """
 import json
+import importlib
 from models.base_model import BaseModel
 from models.state import State
 from models.city import City
@@ -45,28 +46,17 @@ class FileStorage:
         """
         Deserializes the JSON file to __objects (only if the file exists).
         """
+        classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    module_name, obj_id = key.split(".")
-                    cls = __import__(module_name, fromlist=[module_name])
-                    if module_name == "base_model":
-                        instance = BaseModel(**value)
-                    elif module_name == "state":
-                        instance = State(**value)
-                    elif module_name == "city":
-                        instance = City(**value)
-                    elif module_name == "amenity":
-                        instance = Amenity(**value)
-                    elif module_name == "place":
-                        instance = Place(**value)
-                    elif module_name == "review":
-                        instance = Review(**value)
-                    elif module_name == "user":
-                        instance = User(**value)
-                    else:
-                        continue
-                    FileStorage.__objects[key] = instance
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    class_name = val['__class__']
+                    if class_name in classes:
+                        self.all()[key] = classes[class_name](**val)
         except FileNotFoundError:
             pass
